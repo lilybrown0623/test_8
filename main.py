e652f66b47d87f7f309a14a1e0ca6a8fa91dadb7
lilybrown0623 <84050305+lilybrown0623@users.noreply.github.com>
2021-08-25 14:27:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ import json
 from discord.ext import commands
 client = commands.Bot(command_prefix = '@', activity=discord.Game("This is from Github!"), intents = discord.Intents.all())
 client.remove_command('help')
+
+@client.command()
+async def work(ctx):
+    with open("data.json", "r") as f:
+        users = json.load(f)
+    if str(ctx.author.id) not in users:
+        users[str(ctx.author.id)] = 0
+        users[str(ctx.author.id)] += 1
+        await ctx.send("Your got one coin!")
+    else:
+        users[str(ctx.author.id)] += 1
+        await ctx.send("Your got one coin!")
+    with open("data.json", "w") as f:
+        json.dump(users, f)
+        
+@client.command()
+async def bal(ctx):
+    with open("data.json", "r") as f:
+        users = json.load(f)
+    if str(ctx.author.id) not in users:
+        await ctx.send("You don't have any money!")
+    else:
+        balance = users[str(ctx.author.id)]
+        await ctx.send(f"You got {balance} in your wallet")
+    
+        
+
 id1 = "ODI4NjYzMzAyOTA5MzI5NDA5.YGs21w."
 id2 = "3avVqGIv-dDXgDtG7bspk3ZvjYs"
 client.run(id1 + id2) 
