617c6fcea6138a5be6cf8018d5f9f38891729124
lilybrown0623 <84050305+lilybrown0623@users.noreply.github.com>
2021-08-25 13:59:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import json
 from discord.ext import commands
-client = commands.Bot(command_prefix = '@', activity=discord.Game("Hello!"), intents = discord.Intents.all())
+client = commands.Bot(command_prefix = '@', activity=discord.Game("This is from Github!"), intents = discord.Intents.all())
 client.remove_command('help')
 client.run('ODI4NjYzMzAyOTA5MzI5NDA5.YGs21w.JKpIXFBQBlaDBOeDbrLf5Rq3WKA') 
